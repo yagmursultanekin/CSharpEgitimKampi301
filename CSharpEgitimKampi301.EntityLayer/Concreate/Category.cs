95269a9e9dac1794b211186e243ae3ada74a8bca
yagmursultanekin <163584968+yagmursultanekin@users.noreply.github.com>
2026-03-08 17:29:24 +0300

    Proje katmanları oluşturuldu.

---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductNames { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+    }
+}
